ed4ea6942d06a87b07376d2717347541f99e33eb
Xa6i <thebasque@gmail.com>
2013-12-01 11:07:01 -0800

    Add search form to page title if no sidebar

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,20 @@
 <div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
+	<?php if (roots_display_sidebar()) : ?>
+	  <h1>
+    	<?php echo roots_title(); ?>
+  	</h1>
+	<?php else : ?>
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-8 col-lg-8">
+			  <h1>
+			    <?php echo roots_title(); ?>
+			  </h1>
+			</div>  
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			  <p>
+			  	<? get_search_form(); ?>
+			  </p>
+			</div>
+		</div>
+	<?php endif; ?>
 </div>
